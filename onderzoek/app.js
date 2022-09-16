efbeffd4b0815b2949ccf788bcfe25c5dd7b9759
Pascal Benink <9018437@student.zadkine.nl>
2022-09-16 17:17:54 +0200

    .

--- onderzoek/app.js
+++ onderzoek/app.js
@@ -1,5 +1,7 @@
 let num =1
 let num1 = 1
+let b1 = true
+let b2 = true
 if (num3) num1==num2 {
     alert("Tomorrow"); 
 }
@@ -8,4 +10,8 @@ if (num3) num1===num2 {
 }
 if (num3) num1>=num2 {
     alert("Yesterday");
+}
+
+if (num3) b1&&b2 {
+    alert("Last Week");
 }
