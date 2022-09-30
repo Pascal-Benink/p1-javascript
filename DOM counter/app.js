c7f0e14d010659ba35e69d65eb3def0b2e54c823
Pascal-Benink <9018437@student.zadkine.nl>
2022-09-30 09:43:28 +0200

    .

--- DOM counter/app.js
+++ DOM counter/app.js
@@ -1,3 +1,5 @@
+let button = document.getElementById ("btn");
+let button2 = document.getElementById ("btn2");
 let title = document.getElementById("title");
 
 let count = 0;
@@ -6,10 +8,16 @@ function counter(){
     count += 1;
     title.innerText = count + " Keer geklikt";
     document.body.style.backgroundColor = "blue";
+    button.style.backgroundColor = "red"
+    button2.style.backgroundColor = "red"
+
 }
 
 function counter2() {
     count -=1
     title.innerText = count + " Keer geklikt";
     document.body.style.backgroundColor = "red";
+    button.style.backgroundColor = "blue"
+    button2.style.backgroundColor = "blue"
 }
+
