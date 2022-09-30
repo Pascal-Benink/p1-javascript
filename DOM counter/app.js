44a05cc76db6f367b326b84b337b09ff6d960f68
Pascal-Benink <9018437@student.zadkine.nl>
2022-09-30 09:06:42 +0200

    .

--- DOM counter/app.js
+++ DOM counter/app.js
@@ -1,5 +1,4 @@
 let button = document.getElementById("btn");
-let button2 = document.getElementById("btn2");
 
 let count = 0;
 
@@ -8,18 +7,4 @@ function clicker(){
     button.innerText = count + " Keer geklikt"
     button.innerText = count + " Keer geklikt";
     button2.innerText = count + " Keer geklikt";
-}
-
-function clicker2(){
-    count -=1;
-    button.innerText = count + " Keer geklikt";
-    button2.innerText = count + " Keer geklikt";
-
-}
-
-function clicker3(){
-    count =0;
-    button.innerText = count + " Keer geklikt";
-    button2.innerText = count + " Keer geklikt";
-
 }
