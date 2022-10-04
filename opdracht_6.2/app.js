572febca2fa85f3dbd39f6b202ba746d2be83237
Pascal-Benink <9018437@student.zadkine.nl>
2022-10-04 11:17:07 +0200

    opdracht_6.2

---
+++ opdracht_6.2/app.js
@@ -0,0 +1,7 @@
+function kleur_aanpassen(){
+    let title = "titel";
+    let element = document.getElementById(title);
+
+    element.style.color = "red";
+    element.innerText = "Inhoud aangepast!"
+}
