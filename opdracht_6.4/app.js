48a7ee029d6c7f173d25e3678bac9b6a60c751a2
Pascal-Benink <9018437@student.zadkine.nl>
2022-10-06 12:55:25 +0200

    opdracht 6.4

--- opdracht_6.4/app.js
+++ opdracht_6.4/app.js
@@ -1,6 +1,19 @@
 function calculate(num, num2) {
     let awnser = num + num2;
-    console.log(awnser)
+    console.log(awnser);
 }
 
 calculate(8, 15);
+
+function multiplyByFive(num3) {
+    let awnser2 = num3 * 5;
+    console.log(awnser2);
+}
+
+multiplyByFive(7);
+
+function calculateMinutes(num4) {
+    let awnser3 = num4 / 60
+    console.log(awnser3)
+}
+calculateMinutes(3600)
