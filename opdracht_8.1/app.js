7acdfad5fef7d78a61cd38b1a6ae197744f24342
Pascal-Benink <9018437@student.zadkine.nl>
2022-10-20 13:18:32 +0200

    opdracht_8.1

--- opdracht_8.1/app.js
+++ opdracht_8.1/app.js
@@ -18,7 +18,7 @@ function add() {
     let x = document.getElementById("getal1").value;
     let y = document.getElementById("getal2").value;
     let test = document.getElementById("test"); 
-    let z = x + y   
+    let z = x + y  
     test.innerText = z
 }
 
