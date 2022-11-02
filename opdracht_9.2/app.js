e50dad47ddb9e02705c97ff55f27646418b7ac0a
Pascal Benink <9018437@student.zadkine.nl>
2022-11-02 11:43:48 +0100

    opdracht_9.2

--- opdracht_9.2/app.js
+++ opdracht_9.2/app.js
@@ -1,17 +1,16 @@
-let closeded = document.querySelector("emojy closed active")
-let opening = document.querySelector("emojy open")
+let closeded = document.querySelector("emojy closed active");
+let opening = document.querySelector("emojy open");
 
 function openFace(){
-    opening.classList.add("active")
-    closeded.classList.remove("active")
-    opening.classList.contains("active")
-    closeded.classList.contains("active")
-}
+    opening.classList.add("active");
+    closeded.classList.remove("active");
+    opening.classList.contains("active");
+    closeded.classList.contains("active");
+};
 function closedFace(){
-    console.log("test")
-    closeded.classList.add("active")
-    opening.classList.remove("active")
-    opening.classList.contains("active")
-    closeded.classList.contains("active")
-}
+    closeded.classList.add("active");
+    opening.classList.remove("active");
+    opening.classList.contains("active");
+    closeded.classList.contains("active");
+};
 // Variablen niet hetzelfed omdat dit niet werkt
