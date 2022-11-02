0904a73531d787f9f1a4dc688ec89f08c37d9a14
Pascal Benink <9018437@student.zadkine.nl>
2022-11-02 11:57:34 +0100

    opdracht_9.2

--- opdracht_9.2/app.js
+++ opdracht_9.2/app.js
@@ -1,16 +1,16 @@
 let closeded = document.querySelector("emojy closed active");
 let opening = document.querySelector("emojy open");
 
-function openFace(){
-    opening.classList.add("active");
-    closeded.classList.remove("active");
-    opening.classList.contains("active");
-    closeded.classList.contains("active");
-};
 function closedFace(){
-    closeded.classList.add("active");
-    opening.classList.remove("active");
-    opening.classList.contains("active");
-    closeded.classList.contains("active");
+    if (closeded.classList.contains("active")) {
+        opening.classList.add("active");
+        closeded.classList.remove("active");
+    }
+};
+function openFace() {
+    if (opening.classList.contains("active")) {
+        opening.classList.add("active");
+        closeded.classList.remove("active");
+    }
 };
 // Variablen niet hetzelfed omdat dit niet werkt
